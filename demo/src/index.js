bee8b8b7b05ac67409c1f249f7b024d2c7b24540
Rajasegar Chandran <rajasegar.c@gmail.com>
2018-10-26 21:25:49 +0530

    Fixing demo page with css

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,36 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./styles.css";
 
-import Example from '../../src'
+import Example from "../../src";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-rethink-datepicker Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <h1>react-rethink-datepicker</h1>
+        <h4> Rethinking the date picker UI with React.js</h4>
+        <p>Making date selection work for humans</p>
+
+        <div className="datepicker-container">
+          <Example />
+        </div>
+        <p>
+          Based on this{" "}
+          <a href="https://uxdesign.cc/rethinking-the-date-picker-ui-99b9dcb303ad">
+            article
+          </a>{" "}
+          by Jason Ford
+        </p>
+        <p>
+          <a href="https://github.com/rajasegar/react-awesome-datepicker-demo">
+            Github
+          </a>
+        </p>
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
